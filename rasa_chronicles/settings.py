e75e9cf0df89ba4bad3dd24c748716a7b057bbb9
erizkiatama <ezfortechnology@gmail.com>
2024-09-27 18:07:13 +0800

    Fix uploaded files into media not static

--- rasa_chronicles/settings.py
+++ rasa_chronicles/settings.py
@@ -128,7 +128,13 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- rasa_chronicles/urls.py
+++ rasa_chronicles/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
 
@@ -12,3 +14,6 @@ urlpatterns = [
 urlpatterns += [
     re_path(r'^.*$', under_construction),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
